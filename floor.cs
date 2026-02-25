2cc9795b3a7c9e4101e17ae0cf31b4d24953f5b7
DreamsofDeveloper <69727407+DreamsofDeveloper@users.noreply.github.com>
2026-02-25 18:38:48 +0300

    Update floor.cs

--- floor.cs
+++ floor.cs
@@ -1,11 +1,11 @@
 public class Floor
 {
 private string collectableItem;
-private List<String>collectableItems = new List<String>();
+private List<string>collectableItems = new List<string>();
 
-    public void ItemsOnTheFloor(string )
+    public void ItemsOnTheFloor(string droppedItem )
     {
-        
+        collectableItems.Add(droppeditem);
         
     }
 
